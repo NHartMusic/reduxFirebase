11eb24aa7b4aecbf7c537931179b345b02427997
Nick Hart <nick.hart@irewardhealth.com>
2019-04-18 18:39:33 +0200

    something was fucked with the react router, could have been my custom setup though

--- src/components/Projects/ProjectSummary.js
+++ src/components/Projects/ProjectSummary.js
@@ -1,20 +1,15 @@
 import React from 'react'
 
-//rainbow HOC
-import Rainbow from '../../hoc/Rainbow'
-
 const ProjectSummary = () => {
-    return (
-        
-            <div className='card z-depth-0 project-summary'>
-                <div className='card-content'>
-                    <span className='card-title'>Project Title</span>
-                    <p>Test1</p>
-                    <p className>April 18th</p>
-                </div>
-            </div>
-        
-    )
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title ">Project title</span>
+        <p>Posted by The Net Ninja</p>
+        <p className="grey-text">3rd September, 2am</p>
+      </div>
+    </div>
+  )
 }
 
-export default Rainbow(ProjectSummary)
+export default ProjectSummary

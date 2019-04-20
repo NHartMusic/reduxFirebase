abcf7518f52d65f2db79e5cebb92d7a0bac6e864
Nick Hart <nhartmusic@gmail.com>
2019-04-20 14:48:08 +0200

    lets hope this shit works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 //component imports
-import Navbar from './components/Layout/Navbar'
+import Navbar from './components/Layouts/Navbar'
 import Dashboard from './components/Dashboard/Dashboard'
 import ProjectDetails from './components/Projects/ProjectDetails'
 import SignIn from './components/Auth/SignIn'

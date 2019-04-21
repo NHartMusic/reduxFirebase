a7755b1053fa018ae8ca7c9c7c60cb67caaed924
Nick Hart <nick.hart@irewardhealth.com>
2019-04-21 13:13:27 +0200

    redux store and reducers are set up

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 
-const store = createStore()
+const store = createStore(rootReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
 

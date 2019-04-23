5e02cfd00c53ffca194736e49a652970f1ddd8e7
Nick Hart <nick.hart@irewardhealth.com>
2019-04-23 10:06:41 +0200

    setting up firebase

---
+++ src/config/fb-config.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyAV8vnI1wGPQhqg4nNMLraAAEsYMWpqf8U",
+    authDomain: "redux-firebase-test-27c75.firebaseapp.com",
+    databaseURL: "https://redux-firebase-test-27c75.firebaseio.com",
+    projectId: "redux-firebase-test-27c75",
+    storageBucket: "redux-firebase-test-27c75.appspot.com",
+    messagingSenderId: "790861257528"
+  };
+  firebase.initializeApp(config);

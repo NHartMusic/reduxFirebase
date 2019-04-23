29b8e2d561cb54e6087e82e794da288f741cba8d
Nick Hart <nick.hart@irewardhealth.com>
2019-04-23 10:12:38 +0200

    firebase config set up

--- src/config/fb-config.js
+++ src/config/fb-config.js
@@ -1,3 +1,7 @@
+ import firebase from 'firebase/app'
+ import 'firebase/firestore'
+ import 'firebase/auth'
+ 
  // Initialize Firebase
  var config = {
     apiKey: "AIzaSyAV8vnI1wGPQhqg4nNMLraAAEsYMWpqf8U",
@@ -6,5 +10,9 @@
     projectId: "redux-firebase-test-27c75",
     storageBucket: "redux-firebase-test-27c75.appspot.com",
     messagingSenderId: "790861257528"
-  };
-  firebase.initializeApp(config);
+  }
+
+  firebase.initializeApp(config)
+  firebase.firestore().settings({ timestampsInSnapshots: true })
+
+  export default firebase

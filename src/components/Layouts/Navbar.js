c3b605eed35ce9742b222df25e2c925d8a596015
Nick Hart <nhartmusic@gmail.com>
2019-04-27 19:24:42 +0200

    changed background and tracking auth status

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -5,22 +5,24 @@ import SignedOutLinks from './SignedOutLinks'
 import { connect } from 'react-redux'
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props
+
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+    console.log(auth)
     return (
         <nav className="nav-wrapper purple accent-3">
             <div className="container">
                 <Link to='/' className="brand-logo">Redux Firebase Test</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {links}
             </div>
         </nav>
     )
 }
 
 const mapStateToProps = (state) => {
-    console.log(state)
     return {
-
+        auth: state.firebase.auth
     }
 }
 
